5561eac29648a6e2287cb95553d42d41b329149b
Adrian Sims <adriansims62781@gmail.com>
2018-01-16 23:56:37 +0000

    Add project files.

---
+++ LinkMaker/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkMaker
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 1)
+                {
+                    throw new Exception("You must supply at least one argument");
+                }
+                foreach (var arg in args)
+                {
+                    bool firstDiv = true;
+                    int divCount = 0;
+                    Console.WriteLine("<!doctype html>");
+                    Console.WriteLine("<html lang=\"en\">");
+                    Console.WriteLine("<head>");
+                    Console.WriteLine("<title>My Links</title>");
+                    Console.WriteLine("<style type='text/css'>");
+                    Console.WriteLine("html, body { height: 100%; }\n " +
+                        "#linklist, #linklist a {\ndisplay: flex;\n   flex-flow: column wrap;\n   max-height: 100%; }\n" +
+                        "#linklist a { margin-right: 2em }");
+                    Console.WriteLine("</style>");
+                    Console.WriteLine("<script>");
+                    Console.WriteLine("</script>");
+                    Console.WriteLine("</head>");
+                    Console.WriteLine("<body>");
+
+                    Console.WriteLine("<div id=\"linklist\">");
+                    foreach (var line in File.ReadLines(arg).Where(x=>x.Length > 0))
+                    {
+                        line.Trim();
+                        if (line[0] == '>')
+                        {
+                            if (firstDiv)
+                            {
+                                firstDiv = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("</div>");
+                            }
+                            divCount++;
+                            Console.WriteLine($"<div>\n    <p class=\"section\">{line.Substring(1).Trim()}</p>");
+                        }
+                        else if (line[0] == '<')
+                        {
+                            Console.WriteLine($"{line}");
+                        }
+                        else
+                        {
+                            var tokens = line.Split();
+                            var link = tokens[0];
+                            var rest = string.Join(" ", tokens.Skip(1));
+                            Console.WriteLine($"<a href=\"{link}\">{rest}</a>");
+                        }
+                    }
+                    if (divCount > 0)
+                    {
+                        Console.WriteLine("</div>");
+                    }
+                    Console.WriteLine("</div>");
+                    Console.WriteLine("</body>");
+                    Console.WriteLine("</html>");
+                }
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add LinkMaker/Program.cs
+git add LinkMaker/App.config
+git add LinkMaker/Properties/AssemblyInfo.cs
+git add LinkMaker/LinkMaker.csproj
+git add LinkMaker.sln
+git add .gitignore
